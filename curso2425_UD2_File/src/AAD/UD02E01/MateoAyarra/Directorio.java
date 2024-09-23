836a83695faaa6301b6735be9b190182e9b24cf6
mateomate1 <mateoayarrabarbero@gmail.com>
2024-09-23 13:48:46 +0200

    23/9/2024

--- curso2425_UD2_File/src/AAD/UD02E01/MateoAyarra/Directorio.java
+++ curso2425_UD2_File/src/AAD/UD02E01/MateoAyarra/Directorio.java
@@ -5,5 +5,19 @@ import java.util.List;
 public class Directorio extends Archivo {
 	
 	public List<Archivo> contenido;
+
+	public Directorio(String absolutePath, String tipo, List<Archivo> contenido) {
+		super(absolutePath, tipo);
+		this.contenido = contenido;
+	}
+	
+	public static void recorrer() {
+		File fichero = new File()
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
 	
 }
