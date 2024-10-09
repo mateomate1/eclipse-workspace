b7d587f49eb3bb3a6fe015dee3b4952093aec44b
mateomate1 <mateoayarrabarbero@gmail.com>
2024-10-09 17:31:31 +0200

    9/10/2024

--- InterfazGrafica/src/Actividad28_MATEO_AYARRA/Pantalla.java
+++ InterfazGrafica/src/Actividad28_MATEO_AYARRA/Pantalla.java
@@ -154,7 +154,7 @@ public class Pantalla extends JFrame{
 
 	try {
 	    for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-	        if ("Windows".equals(info.getName())) {
+	        if ("Metal".equals(info.getName())) {
 	            javax.swing.UIManager.setLookAndFeel(info.getClassName());
 	            break;
 	        }
