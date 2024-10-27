ef1f738885a206c994e06744183d0c5679646576
mateomate1 <mateoayarrabarbero@gmail.com>
2024-10-27 18:13:13 +0100

    27/10/2024

--- DnDCharacter/main/main/Main.java
+++ DnDCharacter/main/main/Main.java
@@ -1,9 +1,13 @@
 package main;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Random random = new Random();
+		System.out.println(random.nextInt(1,21));
 
 	}
 
