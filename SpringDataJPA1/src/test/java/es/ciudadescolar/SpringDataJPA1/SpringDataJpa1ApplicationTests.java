dfe3d38694b969b7c4cf74162a8c9ac2d07067f2
mateomate1 <mateoayarrabarbero@gmail.com>
2025-02-13 14:01:35 +0100

    13/2/2025

---
+++ SpringDataJPA1/src/test/java/es/ciudadescolar/SpringDataJPA1/SpringDataJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ciudadescolar.SpringDataJPA1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
